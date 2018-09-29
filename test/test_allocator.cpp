ff800535e1104424c849f6c66d96a1e2f6cef0f0
artbataev <artbataev@gmail.com>
2018-09-29 20:59:31 +0300

    Added more tests

--- test/test_allocator.cpp
+++ test/test_allocator.cpp
@@ -8,6 +8,17 @@ TEST(TestAllocator, TestEmpty) {
     ASSERT_EQ(map_with_custom_allocator.size(), 0);
 }
 
+TEST(TestAllocator, TestElements) {
+    std::map<int, int, std::less<>,
+            permanent_allocator<std::pair<const int, int>, 20>> map_with_custom_allocator;
+    for(int i = 0; i < 20; i++) {
+        map_with_custom_allocator[i] = i * 100;
+    }
+    for(int i = 0; i < 20; i++) {
+        ASSERT_EQ(map_with_custom_allocator[i], i * 100);
+    }
+}
+
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
